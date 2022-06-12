cc91efb67400ae31ed422e0b9e20e3698a1af52d
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-12 12:34:30 +0200

    Удалил файл square из проекта, добавил новую функцию для нарезания

--- first_project/strings.py
+++ first_project/strings.py
@@ -2,5 +2,10 @@ def upper_text(text):
     return text.upper()
 
 
+def cut_text(text):
+    return text[:5]
+
+
 if __name__ == '__main__':
     print(upper_text('Hello World'))
+    print(cut_text('Hello Jhony'))
