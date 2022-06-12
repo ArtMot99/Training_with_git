4ebab5f7914fa0cba22fc0986eff2e2ccf24dd77
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-12 12:04:27 +0200

    Добавил новый модуль string, а так же функцию mult в nums

--- first_project/nums.py
+++ first_project/nums.py
@@ -6,6 +6,11 @@ def minus(a, b):
     return a - b
 
 
+def mul(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(plus(2, 4))
     print(minus(2, 4))
+    print(mul(2, 4))
---
+++ first_project/strings.py
@@ -0,0 +1,6 @@
+def upper_text(text):
+    return text.upper()
+
+
+if __name__ == '__main__':
+    print(upper_text('Hello World'))
